5af95bafcd1e027348dfd9a8d4eeb2840545002e
Anavarren1997 <anavarren@alumnos.unex.es>
2022-10-10 19:42:49 +0200

    ultimo commit

--- app/models/index.js
+++ app/models/index.js
@@ -12,5 +12,16 @@ db.tutorials = require("./tutorial.model")(mongoose);
  * Aquí se insertan las demás colecciones?xd
  */
 
+
+/* 
+create a new Tutorial: object.save()
+find a Tutorial by id: findById(id)
+retrieve all Tutorials: find()
+update a Tutorial by id: findByIdAndUpdate(id, data)
+remove a Tutorial: findByIdAndRemove(id)
+remove all Tutorials: deleteMany()
+find all Tutorials by title: find({ title: { $regex: new RegExp(title), $options: “i” } })
+*/
+
 module.exports = db;
 
